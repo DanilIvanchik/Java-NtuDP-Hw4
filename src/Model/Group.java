aff11a60744b2198a0933f5b55dc8e43244626e0
Daniil_Ivanchyk <57773641+DanilIvanchik@users.noreply.github.com>
2022-11-18 01:22:38 +0200

    Update Group.java

--- src/Model/Group.java
+++ src/Model/Group.java
@@ -1,6 +1,7 @@
 package Model;
 
 import java.util.List;
+import java.util.Objects;
 
 public class Group extends Department{
     private String nameOfGroup;
@@ -33,4 +34,18 @@ public class Group extends Department{
     public String toString() {
         return "\nGroup name: " + nameOfGroup + "\nHead of group:" + headOfGroup + "\nStudents:\n" + getStudents();
     }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Group group = (Group) o;
+        return Objects.equals(nameOfGroup, group.nameOfGroup) && Objects.equals(headOfGroup, group.headOfGroup) && Objects.equals(students, group.students);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(nameOfGroup, headOfGroup, students);
+    }
+
 }
